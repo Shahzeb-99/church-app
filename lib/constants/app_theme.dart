2a466af05df078cc2fe7b6e133124cd1ef62281c
Shahzeb Iftikhar <shahzebiftikharmalicc@gmail.com>
2023-10-23 11:10:36 +0500

    initial commit, app theme update, firebase initialized, package and app name updated.

---
+++ lib/constants/app_theme.dart
@@ -0,0 +1,182 @@
+/// Creating custom color palettes is part of creating a custom app. The idea is to create
+/// your class of custom colors, in this case `CompanyColors` and then create a `ThemeData`
+/// object with those colors you just defined.
+///
+/// Resource:
+/// A good resource would be this website: http://mcg.mbitson.com/
+/// You simply need to put in the colour you wish to use, and it will generate all shades
+/// for you. Your primary colour will be the `500` value.
+///
+/// Colour Creation:
+/// In order to create the custom colours you need to create a `Map<int, Color>` object
+/// which will have all the shade values. `const Color(0xFF...)` will be how you create
+/// the colours. The six character hex code is what follows. If you wanted the colour
+/// #114488 or #D39090 as primary colours in your theme, then you would have
+/// `const Color(0x114488)` and `const Color(0xD39090)`, respectively.
+///
+/// Usage:
+/// In order to use this newly created theme or even the colours in it, you would just
+/// `import` this file in your project, anywhere you needed it.
+/// `import 'path/to/theme.dart';`
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppThemeData {
+  static const _lightFillColor = Colors.black;
+  static const _darkFillColor = Colors.white;
+
+  static final Color _lightFocusColor = Colors.black.withOpacity(0.12);
+  static final Color _darkFocusColor = Colors.white.withOpacity(0.12);
+
+  //static ThemeData lightThemeData = themeData(lightColorScheme, _lightFocusColor);
+  static ThemeData lightThemeData = FlexThemeData.light(
+    textTheme: _textTheme,
+    scheme: FlexScheme.red,
+
+  ).copyWith(
+    appBarTheme: AppBarTheme(titleTextStyle: _textTheme.headlineMedium,elevation: 0 ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ),
+    snackBarTheme: SnackBarThemeData(
+      behavior: SnackBarBehavior.floating,
+    ),
+    inputDecorationTheme: _inputDecorationTheme,
+  );
+
+  static ThemeData darkThemeData = FlexThemeData.dark(textTheme: _textTheme, scheme: FlexScheme.red, ).copyWith(
+      appBarTheme: AppBarTheme(titleTextStyle: _textTheme.headlineMedium ,elevation: 0),
+      snackBarTheme: SnackBarThemeData(
+        behavior: SnackBarBehavior.floating,
+      ),
+      outlinedButtonTheme:
+      OutlinedButtonThemeData(style: OutlinedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+      elevatedButtonTheme:
+      ElevatedButtonThemeData(style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+      inputDecorationTheme: _inputDecorationTheme);
+
+  static ThemeData themeData(ColorScheme colorScheme, Color focusColor) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      textTheme: _textTheme,
+      // Matches manifest.json colors and background color.
+      primaryColor: const Color(0xFF030303),
+      appBarTheme: AppBarTheme(
+        backgroundColor: colorScheme.background,
+        elevation: 0,
+        iconTheme: IconThemeData(color: colorScheme.primary),
+      ),
+      iconTheme: IconThemeData(color: colorScheme.onPrimary),
+      canvasColor: colorScheme.background,
+      scaffoldBackgroundColor: colorScheme.background,
+      highlightColor: Colors.transparent,
+      focusColor: focusColor,
+
+      snackBarTheme: SnackBarThemeData(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Color.alphaBlend(
+          _lightFillColor.withOpacity(0.80),
+          _darkFillColor,
+        ),
+        contentTextStyle: _textTheme.titleMedium!.apply(color: _darkFillColor),
+      ),
+    );
+  }
+
+  static const ColorScheme lightColorScheme = ColorScheme(
+    primary: Color(0xFFd21e1d),
+    primaryContainer: Color(0xFF9e1718),
+    secondary: Color(0xFFEFF3F3),
+    secondaryContainer: Color(0xFFFAFBFB),
+    background: Color(0xFFE6EBEB),
+    surface: Color(0xFFFAFBFB),
+    onBackground: Colors.white,
+    error: _lightFillColor,
+    onError: _lightFillColor,
+    onPrimary: _lightFillColor,
+    onSecondary: Color(0xFF322942),
+    onSurface: Color(0xFF241E30),
+    brightness: Brightness.light,
+  );
+
+  static const ColorScheme darkColorScheme = ColorScheme(
+    primary: Color(0xFFFF8383),
+    primaryContainer: Color(0xFF1CDEC9),
+    secondary: Color(0xFF4D1F7C),
+    secondaryContainer: Color(0xFF451B6F),
+    background: Color(0xFF241E30),
+    surface: Color(0xFF1F1929),
+    onBackground: Color(0x0DFFFFFF),
+    // White with 0.05 opacity
+    error: _darkFillColor,
+    onError: _darkFillColor,
+    onPrimary: _darkFillColor,
+    onSecondary: _darkFillColor,
+    onSurface: _darkFillColor,
+    brightness: Brightness.dark,
+  );
+
+  static const _regular = FontWeight.w400;
+  static const _medium = FontWeight.w500;
+  static const _semiBold = FontWeight.w600;
+  static const _bold = FontWeight.w700;
+
+  static final InputDecorationTheme _inputDecorationTheme = InputDecorationTheme(
+    filled: true,
+    //fillColor: Colors.white,
+    prefixIconColor: Colors.red,
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    // Set the border radius for the text field
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.0),
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.0),
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.0),
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12.0),
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+    ),
+    // Set the border color for the text field
+    enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+      borderRadius: BorderRadius.circular(12.0),
+    ),
+    // Set the focused border color for the text field
+    focusedBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+      borderRadius: BorderRadius.circular(12.0),
+    ),
+  );
+
+  static final TextTheme _textTheme = TextTheme(
+    headlineMedium: GoogleFonts.montserrat(fontWeight: _bold, fontSize: 20.0),
+    bodySmall: GoogleFonts.oswald(fontWeight: _semiBold, fontSize: 16.0),
+    headlineSmall: GoogleFonts.oswald(fontWeight: _medium, fontSize: 16.0),
+    titleMedium: GoogleFonts.montserrat(fontWeight: _medium, fontSize: 16.0),
+    labelSmall: GoogleFonts.montserrat(fontWeight: _medium, fontSize: 12.0),
+    bodyLarge: GoogleFonts.montserrat(fontWeight: _regular, fontSize: 14.0),
+    titleSmall: GoogleFonts.montserrat(fontWeight: _medium, fontSize: 14.0),
+    bodyMedium: GoogleFonts.montserrat(fontWeight: _regular, fontSize: 16.0),
+    titleLarge: GoogleFonts.montserrat(fontWeight: _bold, fontSize: 16.0),
+    labelLarge: GoogleFonts.montserrat(fontWeight: _semiBold, fontSize: 14.0),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:saintpopekerollosvi/ui/my_app.dart';
+
+import 'di/components/service_locator.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await setPreferredOrientations();
+  await setupLocator();
+  return runZonedGuarded(() async {
+    runApp(MyApp());
+  }, (error, stack) {
+    print(stack);
+    print(error);
+  });
+}
+
+Future<void> setPreferredOrientations() {
+  return SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+}
